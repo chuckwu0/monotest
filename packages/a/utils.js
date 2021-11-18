2ecf30a67293bf4a60a51952a16d3faafab8a48f
wuke <wuke@taou.com>
2021-11-18 14:49:56 +0800

    feat(ds): nop
    affects: a
    
    nop

--- packages/a/utils.js
+++ packages/a/utils.js
@@ -1,4 +1,4 @@
 const semver  =require('semver');
 module.exports.aUtil   =  function (){
-  console.log('from a util semver.gt',semver.gt('1.0.0','10.0.0'));
+  console.log('from a util semver.gt  go ',semver.gt('1.0.0','10.0.0'));
 }
